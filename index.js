2bbbb452625abe36b9b191459effbddb6f370645
Erin Penner <epenner0522@gmail.com>
2018-08-01 19:53:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function theBeatlesPlay(musicians, instruments) {
   var newArray = {};
   
   for (var i=0; i < musicians.length; i++) {
-    newArray[i] = `${musicians[i]} plays ${instruments[i]}`;
+    newArray.push(`${musicians[i]} plays ${instruments[i]}`);
   }
   return newArray;
 }
